dda61a946ed16701c00c1240ba5526cf10eb30c4
michalwitek1232 <michalwitek1232@gmail.com>
2020-02-19 12:29:15 +0100

    naprawione

--- WebAPI_1/Startup.cs
+++ WebAPI_1/Startup.cs
@@ -26,7 +26,7 @@ namespace WebAPI_1
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public object ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(s => s.UseSqlServer("ConnectionString"));
 
