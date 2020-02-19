b9e2f10da49c0dcd7488d245dca30b6f9566088f
michalwitek1232 <michalwitek1232@gmail.com>
2020-02-19 13:18:55 +0100

    Oprogramowane metody :3

--- WebAPI_1/Controllers/DbGetController.cs
+++ WebAPI_1/Controllers/DbGetController.cs
@@ -2,22 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Providers.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI_1.Data;
+using WebAPI_1.Models;
 
 namespace WebAPI_1.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DbGetController : ControllerBase
+    public class DataCTX : ControllerBase
     {
+        private DataContext _context; //pole prywatne
+
+        public DataCTX(DataContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
 
-        public string str()
+        public ActionResult<IEnumerable<string>> Get()
         {
-            return "Hello World!";
+            var values = _context.Users.ToList();
+            return Ok(values); //zwracanie
         }
 
+        [HttpGet("{id}")]
+
+        public IActionResult GetValue(int id)
+        {
+            var values = _context.Users.FirstOrDefault(x => x.id == id);
+            return Ok(values); //zwracanie
+        }
+
+        //dodawanie do tabeli
+
+        [HttpPost]
+
+        public void AddValue([FromBody] Users user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges(); //z modelu
+
+        }
+
+        //Edycja wartości
+
+        [HttpPut("{id}")]
+
+        public void EditValue(int id, [FromBody] Users user)
+        {
+            var data = _context.Users.Find(id);
+            data.Imie = user.Imie;
+            _context.Users.Update(data);
+            _context.SaveChanges();
+        }
+
+        //usuwanie wartości
+
+        [HttpDelete("{id}")]
+
+        public void DeleteValue(int id)
+        {
+            var data = _context.Users.Find(id);
+            _context.Users.Remove(data);
+            _context.SaveChanges();
+        }
     }
 }
---
+++ WebAPI_1/Controllers/Value.cs
@@ -0,0 +1,6 @@
+﻿namespace WebAPI_1.Controllers
+{
+    public class User
+    {
+    }
+}
