54be5f7557fcaa78febd0c1583f573bec0dbbafc
michalwitek1232 <michalwitek1232@gmail.com>
2020-03-05 12:13:41 +0100

    asdasdas

---
+++ WebAPI_1/Migrations/20200305103226_outpost.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI_1.Migrations
+{
+    public partial class outpost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
